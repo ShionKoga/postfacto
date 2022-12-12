112e894f0a6aa54b62c1518a5151d48e50032e06
Shion Koga <shion_koga@mail.toyota.co.jp>
2022-12-12 21:03:03 +0900

    ServerとWebを作成
    Signed-off-by: Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
    Co-authored-by: Yuta Shoji <yuta_shoji@mail.toyota.co.jp>

---
+++ server/src/main/kotlin/com/dig/postfacto/PostfactoApplication.kt
@@ -0,0 +1,11 @@
+package com.dig.postfacto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PostfactoApplication
+
+fun main(args: Array<String>) {
+	runApplication<PostfactoApplication>(*args)
+}
---
+++ server/src/test/kotlin/com/dig/postfacto/PostfactoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dig.postfacto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostfactoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
